3f8f2d187ea4aabb97498b9e9512c68eb54607d7
Ralf Sternberg <rsternberg@eclipsesource.com>
2016-01-14 12:02:18 +0100

    Import bundles from github
    Import bundles from rap-d3charts, extensible-charts branch [1].
    
    Remove basic charts in favor of nvd3 charts.
    Raise bundle versions to 0.1.0.
    Adjust bundle names and vendor for RAP Incubator project.
    
    
    [1] https://github.com/eclipsesource/rap-d3charts/tree/extensible-charts
    
    Change-Id: I8694d7ecf5c0403c845f8119ea524383a1a4ea92

---
+++ tests/org.eclipse.rap.addons.chart.test/js/chart/rap-mock.js
@@ -0,0 +1,46 @@
+rap = {
+
+  _listeners: {},
+
+  on: function( eventType, listener ) {
+    if( !this._listeners[ eventType ] ) {
+      this._listeners[ eventType ] = [];
+    }
+    this._listeners[ eventType ].push( listener );
+  },
+
+  off: function( eventType, listener ) {
+    if( this._listeners[ eventType ] ) {
+      var listeners = this._listeners[ eventType ];
+      var index = -1;
+      for( var i = 0; i < listeners.length; i++ ) {
+        if( listeners[ i ] === listener ) {
+          index = i;
+        }
+      }
+      if( index !== -1 ) {
+        listeners.splice( i, 1 );
+      }
+    }
+  },
+
+  registerTypeHandler: function() {
+  },
+
+  setup: function() {
+    this._listeners = {};
+  },
+
+  notify: function( eventType ) {
+    if( this._listeners[ eventType ] ) {
+      var listeners = this._listeners[ eventType ];
+      for( var i = 0; i < listeners.length; i++ ) {
+        listeners[ i ]();
+      }
+    }
+  }
+
+};
+
+rwt = {
+};
