f3309976d590dcb64e56ee7f578dc32f2b0e4ff1
Ralf Sternberg <rsternberg@eclipsesource.com>
2016-01-14 12:02:18 +0100

    Import bundles from github
    Import bundles from rap-d3charts, extensible-charts branch [1].
    
    Raise bundle versions to 0.1.0.
    Adjust bundle names and vendor for RAP Incubator project.
    
    [1] https://github.com/eclipsesource/rap-d3charts/tree/extensible-charts
    
    Change-Id: I8694d7ecf5c0403c845f8119ea524383a1a4ea92

---
+++ bundles/org.eclipse.rap.addons.chart/js/chart/basic/bar.js
@@ -0,0 +1,87 @@
+/*******************************************************************************
+ * Copyright (c) 2013, 2016 EclipseSource and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Ralf Sternberg - initial API and implementation
+ ******************************************************************************/
+
+rwt.chart.register("basic-bar", function( widget ) {
+
+  var xScale = d3.scale.linear().domain( [ 0, 1 ] );
+  var config = {
+    width: 0,
+    height: 0,
+    margin: 0,
+    barWidth: 25,
+    spacing: 2
+  };
+
+  function render( selection ) {
+    xScale.range( [ 0, config.width - config.margin * 2 ] );
+    var items = selection.selectAll( "g.item" ).data( selection.datum() );
+    createElements( items.enter() );
+    updateElements( items );
+    removeElements( items.exit() );
+  }
+
+  rwt.chart.addConfigOptions( render, config );
+
+  return render;
+
+  function createElements( selection ) {
+    var items = selection.append( "svg:g" )
+      .attr( "class", "item" )
+      .attr( "opacity", 1.0 );
+    items.on( "click", function( datum, index ) { widget.notifySelection( index ); } );
+    // createBars
+    items.append( "svg:rect" )
+      .attr( "x", config.margin )
+      .attr( "y", function( item, index ) { return getOffset( index ); } )
+      .attr( "width", 0 )
+      .attr( "height", config.barWidth )
+      .attr( "fill", function( item ) { return item.color || "#000"; } );
+    // createTexts
+    items.append( "svg:text" )
+      .attr( "x", config.margin )
+      .attr( "y", function( item, index ) { return getOffset( index ) + config.barWidth / 2; } )
+      .attr( "text-anchor", "left" )
+      .attr( "dy", ".35em" )
+      .style( "font-family", "sans-serif" )
+      .style( "font-size", "11px" );
+  }
+
+  function updateElements( selection ) {
+    // updateBars
+    selection.select( "rect" )
+      .transition()
+      .duration( 1000 )
+      .attr( "y", function( item, index ) { return getOffset( index ); } )
+      .attr( "width", function( item ) { return xScale( item.value || 0 ); } )
+      .attr( "height", config.barWidth )
+      .attr( "fill", function( item ) { return item.color || "#000"; } );
+    // updateTexts
+    selection.select( "text" )
+      .transition()
+      .duration( 1000 )
+      .attr( "x", function( item ) { return config.margin + 6 + xScale( item.value || 0 ); } )
+      .attr( "y", function( item, index ) { return getOffset( index ) + config.barWidth / 2; } )
+      .text( function( item ) { return item.text || ""; } );
+  }
+
+  function removeElements( selection ) {
+    selection
+      .transition()
+      .duration( 400 )
+      .attr( "opacity", 0.0 )
+      .remove();
+  }
+
+  function getOffset( index ) {
+    return config.margin + index * ( config.barWidth + config.spacing );
+  }
+
+});
---
+++ bundles/org.eclipse.rap.addons.chart/js/chart/basic/stream.js
@@ -0,0 +1,87 @@
+/*******************************************************************************
+ * Copyright (c) 2013, 2016 EclipseSource and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Ralf Sternberg - initial API and implementation
+ ******************************************************************************/
+
+rwt.chart.register("basic-stream", function( widget ) {
+
+  var stack = d3.layout.stack()
+    .offset( "wiggle" )
+    .values( function( d ) { return d.values; } );
+  var area = d3.svg.area();
+  var config = {
+    width: 0,
+    height: 0,
+    margin: 0
+  };
+
+  function render( selection ) {
+    var width = config.width - config.margin * 2;
+    var height = config.height - config.margin * 2;
+    var xScale = d3.scale.linear()
+      .domain( [ 0, 17 ] )
+      .range( [ config.margin, width ] );
+    var yScale = d3.scale.linear()
+      .domain( [ 0, 160 ] )
+      .range( [ height, config.margin ] );
+    area
+      .x( function( d ) { return xScale( d.x ); } )
+      .y0( function( d ) { return yScale( d.y0 ); } )
+      .y1( function( d ) { return yScale( d.y0 + d.y ); } );
+    var dataItems = selection.datum().map( function( item ) {
+      return {
+        item: item,
+        values: ( item.values || []).map( function( value, index ) {
+          return { x: index, y: value };
+        } )
+      };
+    } );
+    var items = selection.selectAll( "g.item" ).data( stack( dataItems ) );
+    createElements( items.enter() );
+    updateElements( items );
+    removeElements( items.exit() );
+  }
+
+  rwt.chart.addConfigOptions( render, config );
+
+  return render;
+
+  function createElements( selection ) {
+    var items = selection.append( "svg:g" )
+      .attr( "class", "item" )
+      .attr( "opacity", 1.0 );
+    items.on( "click", function( datum, index ) { widget.notifySelection( index ); } );
+    // createStreams
+    items.append( "svg:path" )
+      .attr( "d", function( d ) { return area( d.values ); } )
+      .style( "fill", function( d ) { return d.item.color || "#000"; } )
+      .append( "svg:title" )
+        .text( function( d ) { return d.item.text || ""; } );
+    // createTexts
+    items.append( "svg:text" )
+      .attr( "text", function( d ) { return d.item.text || ""; } );
+  }
+
+  function updateElements( selection ) {
+    // updateStreams
+    selection.select( "path" )
+      .transition()
+      .duration( 1000 )
+      .attr( "d", function( d ) { return area( d.values ); } );
+  }
+
+  function removeElements( selection ) {
+    selection
+      .transition()
+      .duration( 400 )
+      .attr( "opacity", 0.0 )
+      .remove();
+  }
+
+});
