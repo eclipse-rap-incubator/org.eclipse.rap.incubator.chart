273f137b6e5e3d25bd645f6ebef68a71aa231a25
Ralf Sternberg <rsternberg@eclipsesource.com>
2016-02-19 13:34:13 +0100

    [WIP] Experimental support for WorldMap
    Using TopoJSON [1].
    
    The chart component is build around a convention suggested by d3 author
    Mike Bostok [2].
    
    [1] https://github.com/mbostock/topojson
    [2] http://bost.ocks.org/mike/chart/
    
    Change-Id: I2b8aefae338f9b24b71d0f2c679993e26dabd6cf

---
+++ bundles/org.eclipse.rap.addons.chart/src/org/eclipse/rap/addons/chart/basic/WorldMapChart.java
@@ -0,0 +1,66 @@
+/*******************************************************************************
+ * Copyright (c) 2016 EclipseSource and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Ralf Sternberg - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.rap.addons.chart.basic;
+
+import static org.eclipse.rap.addons.chart.internal.ColorUtil.toHtmlString;
+
+import java.util.Map;
+
+import org.eclipse.rap.addons.chart.MapChart;
+import org.eclipse.rap.json.JsonArray;
+import org.eclipse.rap.json.JsonObject;
+import org.eclipse.swt.graphics.RGB;
+import org.eclipse.swt.widgets.Composite;
+
+
+public class WorldMapChart extends MapChart {
+
+  private boolean showGraticule;
+
+  public WorldMapChart( Composite parent, int style ) {
+    super( parent, style, "topojson-world" );
+    requireJs( registerResource( "chart/topojson/topojson-world.js" ) );
+    requireCss( registerResource( "resources/topojson-world.css" ) );
+    registerResource( "resources/world-110m.json" );
+  }
+
+  public void setColors( Map<String, RGB> colors ) {
+    checkWidget();
+    JsonObject json = new JsonObject();
+    for( String code : colors.keySet() ) {
+      json.add( code, toHtmlString( colors.get( code ) ) );
+    }
+    setOption( "colors", json );
+  }
+
+  public void setColors( RGB[] colors ) {
+    checkWidget();
+    JsonArray json = new JsonArray();
+    for( RGB color : colors ) {
+      json.add( toHtmlString( color ) );
+    }
+    setOption( "colors", json );
+  }
+
+  public void setShowGraticule( boolean show ) {
+    checkWidget();
+    if( show != showGraticule ) {
+      showGraticule = show;
+      setOption( "showGraticule", show );
+    }
+  }
+
+  public boolean getShowGraticule() {
+    checkWidget();
+    return showGraticule;
+  }
+
+}
