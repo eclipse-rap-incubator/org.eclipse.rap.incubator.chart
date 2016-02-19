9670c80304c2015d81bbd4edc22cd8656b7366bf
Ralf Sternberg <rsternberg@eclipsesource.com>
2016-02-19 13:34:13 +0100

    [WIP] Experimental support for WorldMap
    Using TopoJSON [1].
    
    The chart component is build around a convention suggested by d3 author
    Mike Bostok [2].
    
    [1] https://github.com/mbostock/topojson
    [2] http://bost.ocks.org/mike/chart/
    
    Change-Id: I2b8aefae338f9b24b71d0f2c679993e26dabd6cf

---
+++ examples/org.eclipse.rap.addons.chart.demo/src/org/eclipse/rap/addons/chart/demo/WorldMapSnippet.java
@@ -0,0 +1,52 @@
+/*******************************************************************************
+ * Copyright (c) 2016 EclipseSource and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Ralf Sternberg - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.rap.addons.chart.demo;
+
+import org.eclipse.rap.addons.chart.basic.WorldMapChart;
+import org.eclipse.rap.rwt.application.AbstractEntryPoint;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
+
+
+public class WorldMapSnippet extends AbstractEntryPoint {
+
+  private WorldMapChart mapChart;
+
+  @Override
+  public void createContents( Composite parent ) {
+    parent.setLayout( new GridLayout() );
+    createMapChart( parent );
+  }
+
+  private void createMapChart( Composite parent ) {
+    mapChart = new WorldMapChart( parent, SWT.NONE );
+    mapChart.setLayoutData( new GridData( SWT.FILL, SWT.FILL, true, true ) );
+    mapChart.addListener( SWT.Selection, new Listener() {
+      @Override
+      public void handleEvent( Event event ) {
+        System.out.println( "Selected country #" + event.index );
+      }
+    } );
+    // TODO: Allow for three-letter country codes
+    // See https://en.wikipedia.org/wiki/ISO_3166-1_alpha-3
+//    Map<String, RGB> colors = new HashMap<>();
+//    colors.put( "100", new RGB( 200, 0, 0 ) ); // BGR
+//    colors.put( "276", new RGB( 200, 0, 0 ) ); // DEU
+//    colors.put( "*", new RGB( 0, 100, 0 ) );
+//    mapChart.setColors( colors );
+    mapChart.setColors( Colors.CAT10_COLORS );
+  }
+
+}
